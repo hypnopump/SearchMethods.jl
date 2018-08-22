d7338655bbb5864f1d2a6cdac05c2fa7904323b0
Eric Alcaide <ericalcaide1@gmail.com>
2018-08-22 18:09:15 +0200

    starting the code w/ uninformed search. Planning to add 5 uninformed, 2 informed and then games (minimax, montecarlo...)

---
+++ tests.jl
@@ -0,0 +1,47 @@
+# To run: `julia bfs_test.jl`
+# Code written entirely by Eric Alcaide.
+# An implementation of a Tree data structure for test purposes.
+
+include("uninformed.jl")
+include("informed.jl")
+
+tree = Dict(
+    1 => [2, 3, 4, 5],
+    2 => [6, 7, 8],
+    3 => [9, 6],
+    4 => [11],
+    5 => [12, 13],
+    6 => [],
+    7 => [14],
+    8 => [],
+    9 => [],
+    10 => [],
+    11 => [15],
+    12 => [],
+    13 => [16, 17, 18],
+    14 => [19, 20],
+    15 => [21, 22],
+    16 => [23],
+    17 => [],
+    18 => [24, 25],
+    19 => [],
+    20 => [],
+    21 => [],
+    22 => [],
+    23 => [26],
+    24 => [],
+    25 => [],
+    26 => [27, 28, 29],
+    27 => [],
+    28 => [],
+    29 => [],
+)
+
+function expand(node, tree)
+    return tree[node]
+end
+
+# Sample test for the BFS algorithm.
+println("BFS Test: ", Search.bfs(1, tree, 90, expand))
+# Sample test for the DFS algorithm.
+println("DFS Test: ", Search.dfs(1, tree, 28, expand))
---
+++ uninformed.jl
@@ -0,0 +1,79 @@
+""" Code written entirely by Eric Alcaide.
+	
+	An implementation of BFS (Breadth-First Search) algorithm
+	in julia for a future package: Search.jl
+"""
+
+module Search
+
+    export bfs, dfs
+
+    function bfs(root, tree, goal, expand)
+        explored = Set([])
+        frontier = [root]
+        # While frontier is active, pop from left (FIFO)
+        while !isempty(frontier)
+        	node = shift!(frontier)
+        	push!(explored, node)
+        	# Check if goal found. Else => continue.
+        	if node==goal
+        		return node
+        	end
+        	# Explore node
+        	for neighbor in expand(node, tree)
+        		if !in(neighbor, explored)
+        			push!(frontier, neighbor)
+        		end
+        	end
+        end
+        # Return false if goal not found.
+        return false
+    end
+
+    function dfs(root, tree, goal, expand)
+        explored = Set([])
+        frontier = [root]
+        # While frontier is active, pop from right (LIFO)
+        while !isempty(frontier)
+        	node = pop!(frontier)
+        	push!(explored, node)
+        	# Check if goal found. Else => continue.
+        	if node==goal
+        		return node
+        	end
+        	# Explore node
+        	for neighbor in expand(node, tree)
+        		if !in(neighbor, explored)
+        			push!(frontier, neighbor)
+        		end
+        	end
+        end
+        # Return false if goal not found.
+        return false
+    end
+
+    function dls(root, tree, goal, expand, depth)
+    	explored = Set([])
+    	# Keep track of node, depth while node in frontier
+        frontier = [[root, 0]
+        # While frontier is active, pop from right (LIFO)
+        while !isempty(frontier)
+        	node = pop!(frontier)
+        	push!(explored, node[1])
+        	# Check if goal found. Else => continue.
+        	if node[1]==goal
+        		return node
+        	end
+        	# Explore node only if depth-admissible
+        	if node[end] < depth
+	        	for neighbor in expand(node[1], tree)
+	        		if !in(neighbor, explored)
+	        			push!(frontier, [neighbor, node[end]+1])
+	        		end
+	        	end
+	        end
+        end
+        # Return false if goal not found.
+        return false
+
+end
